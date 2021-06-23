276d54ea7f017747b27b91a35f485e41f1e10b4e
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-06-23 17:25:18 +0900

    Add files via upload

---
+++ Chapter01/src/main/java/com/rubypaper/domain/BoardVO.java
@@ -0,0 +1,23 @@
+package com.rubypaper.domain;
+
+import java.util.Date;
+
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+
+import lombok.Data;
+
+//@Getter
+//@Setter
+//@ToString
+
+@Data
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date createDate = new Date();
+	private int cnt;
+}
---
+++ Chapter03/src/main/java/com/rubypaper/PropertiesTest.java
@@ -0,0 +1,19 @@
+package com.rubypaper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertiesTest {
+	
+	private BoardVO boardVO = null;
+	
+	@Test
+	public void testMethod {
+		
+	}
+
+}
